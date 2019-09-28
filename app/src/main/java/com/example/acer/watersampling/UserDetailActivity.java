a29f714cb241c057cd8d4b4a5da7cda7374388b5
xin <xin@qq.com>
2019-09-28 18:36:01 +0800

    water_sampling_second

--- app/src/main/java/com/example/acer/watersampling/UserDetailActivity.java
+++ app/src/main/java/com/example/acer/watersampling/UserDetailActivity.java
@@ -2,13 +2,18 @@ package com.example.acer.watersampling;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
@@ -17,7 +22,16 @@ import com.example.acer.watersampling.bean.Msg;
 import com.example.acer.watersampling.bean.User;
 import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.XMLFormatter;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class UserDetailActivity extends AppCompatActivity implements View.OnClickListener {
@@ -94,7 +108,7 @@ public class UserDetailActivity extends AppCompatActivity implements View.OnClic
                 break;
 //            查看所需要的全部工具
             case R.id.myTools_button:
-
+                showAllTools();
                 break;
 //                领取任务
             case R.id.getTask_button:
@@ -104,6 +118,63 @@ public class UserDetailActivity extends AppCompatActivity implements View.OnClic
 
 
         }
+    }
+    private void showAllTools(){
+        OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url("http://10.0.1.38:8080/water_sampling/task/getAllTools?userId=" + userId)
+                .get()
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String s = response.body().string();
+                Log.i("tag",s+"getAllTools");
+                Msg msg = new Gson().fromJson(s, Msg.class);
+                Object totalBottles = msg.getData().get("totalBottles");
+                String bottleTypeMap_str = new Gson().toJson(msg.getData().get("bottleTypeMap"));
+                Log.i("tag",bottleTypeMap_str+":"+"getAllTools");
+                Map<String, Integer> bottleTypeMap_map = JSON.parseObject(bottleTypeMap_str, new TypeReference<Map<String, Integer>>() {
+                });
+                Log.i("tag",bottleTypeMap_str+":getAllTools"+bottleTypeMap_map.toString());
+                String reagentTypeMap_str = new Gson().toJson(msg.getData().get("reagentTypeMap"));
+                Map<String, Integer> reagentTypeMap_map = JSON.parseObject(reagentTypeMap_str, new TypeReference<Map<String, Integer>>() {
+                });
+                final View view = View.inflate(UserDetailActivity.this, R.layout.view_tools, null);
+               TextView bottleTypeNum_textView_tools = view.findViewById(R.id.bottleTypeNum_textView_tools);
+               TextView bottles_tools = view.findViewById(R.id.bottles_tools);
+               TextView reagentTextView = view.findViewById(R.id.reagent_textView_tools);
+                StringBuffer bottleType_stringBuffer = new StringBuffer();
+                for (Map.Entry<String, Integer> entry:bottleTypeMap_map.entrySet()){
+                    bottleType_stringBuffer.append(entry.getKey()+"：\t"+entry.getValue()+"\n");
+                }
+                bottles_tools.setText("总共需要的瓶子个数："+String.valueOf(totalBottles));
+                StringBuffer reagentType_stringBuffer = new StringBuffer();
+                for (Map.Entry<String, Integer> entry:reagentTypeMap_map.entrySet()){
+                    reagentType_stringBuffer.append(entry.getKey()+"：\t"+entry.getValue()+"\n");
+                }
+                reagentTextView.setText(reagentType_stringBuffer.toString());
+                bottleTypeNum_textView_tools.setText(bottleType_stringBuffer.toString());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        new AlertDialog.Builder(UserDetailActivity.this)
+                                .setTitle("您所需要的瓶子和试剂：")
+                                .setView(view)
+                                .setPositiveButton("确定",null)
+                                .show();
+                    }
+                });
+
+
+            }
+        });
+
     }
 
     public class MyLocationListener extends BDAbstractLocationListener {
